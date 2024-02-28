e9e86efa9609c6d9eaa4594e4130d0b2ac667dad
onelivetech <87318539+onelivetech@users.noreply.github.com>
2024-02-28 15:17:08 -0500

    homepage branded updates

--- app/components/HeroVideo.jsx
+++ app/components/HeroVideo.jsx
@@ -5,11 +5,11 @@ function HeroVideo() {
     return (
         <div>
             <div className="relative">
-                <video className="w-full h-65xvh object-cover" controls autoPlay loop muted>
+                <video className="w-full h-65vh object-cover" controls autoPlay loop muted>
                     <source src={olvideo} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
-                {/* <div className="absolute inset-0 text-white bg-black bg-opacity-50 p-8">
+                <div className="absolute inset-0 text-white bg-black bg-opacity-50 p-8">
 
                     <div className='absolute bottom-3 mb-2.5'>
                         <h2 className="text-3xl font-bold sm:text-5xl">
@@ -22,7 +22,7 @@ function HeroVideo() {
 
                         <div className="mt-8 flex flex-wrap justify-start gap-4">
                             <a
-                                className="block rounded-full border border-pink-600 bg-pink-600 px-12 py-3 text-sm font-medium text-white hover:bg-white hover:text-pink-600 focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                                className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 block rounded-full border border-pink-600 bg-pink-600 px-12 py-3 text-sm font-medium text-white hover:bg-white hover:text-pink-600 focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
                                 href="#" 
                             >
                                 Get Started
@@ -37,7 +37,7 @@ function HeroVideo() {
                         </div>                        
                     </div>
                     
-                </div> */}
+                </div>
             </div>
         </div>
     );
