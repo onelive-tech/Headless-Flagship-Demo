a215b2972b427bdc2d4f883b3437a586af452e73
Billy-Zimmerman <130078404+Billy-Zimmerman@users.noreply.github.com>
2024-03-05 09:28:14 -1000

    added what links I could to the footer, working on styles for at tablet and below to center the footer content, pushing as a good place to stash to if needed, so far so good

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -25,18 +25,22 @@ export default function FooterLayout() {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+      <div className="mx-auto max-w-7xl px-6 pb-8 text-center	pt-16 sm:pt-24 lg:text-left lg:px-8 lg:pt-32">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-          <div className="space-y-8">
-            <img
+          <div className="flex items-center justify-center space-y-8 lg:justify-start pb-2">
+          <img
               className="h-7"
               src={Logo}
-              alt="Company name"
+              alt="Onelive Logo"
             />
+
+
+          </div>
+          <div className="justify-center space-y-8">
             <p className="text-sm leading-6 text-gray-300">
               The end-to-end ecommerce partner for DTC brands.
             </p>
-            <div className="flex space-x-6">
+            <div className="flex mx-auto lg:mx-0 space-x-6">
               {navigation.social.map((item) => (
                 <a key={item.name} href={item.href} className="text-gray-500 hover:text-gray-400">
                   <span className="sr-only">{item.name}</span>
@@ -207,28 +211,28 @@ function activeLinkStyle({isActive, isPending}) {
 
 const navigation = {
   solutions: [
-    { name: 'Marketing', href: '#' },
-    { name: 'Analytics', href: '#' },
-    { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
+    { name: 'Marketing?', href: '#' },
+    { name: 'Analytics?', href: '#' },
+    { name: 'Commerce?', href: '#' },
+    { name: 'Insights?', href: '#' },
   ],
   support: [
-    { name: 'Pricing', href: '#' },
-    { name: 'Documentation', href: '#' },
-    { name: 'Guides', href: '#' },
-    { name: 'API Status', href: '#' },
+    { name: 'Pricing?', href: '#' },
+    { name: 'Documentation?', href: '#' },
+    { name: 'Guides?', href: '#' },
+    { name: 'API Status?', href: '#' },
   ],
   company: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' },
+    { name: 'About', href: 'https://www.onelive.com/about' },
+    { name: 'Blog', href: '/blogs/news' },
+    { name: 'Jobs?', href: '#' },
+    { name: 'Press?', href: '#' },
+    { name: 'Partners', href: 'https://www.onelive.com/shopify-partner' },
   ],
   legal: [
-    { name: 'Claim', href: '#' },
-    { name: 'Privacy', href: '#' },
-    { name: 'Terms', href: '#' },
+    { name: 'Claim?', href: '#' },
+    { name: 'Privacy', href: 'https://www.onelive.com/privacy' },
+    { name: 'Terms', href: 'https://www.onelive.com/terms' },
   ],
   social: [
     {
@@ -263,6 +267,14 @@ const navigation = {
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
+        </svg>
+      ),
+    },
+    {
+      name: 'Linkedin',
+      href: 'https://www.linkedin.com/company/one-live',
+      icon: (props) => (
+        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
         </svg>
       ),
     },
